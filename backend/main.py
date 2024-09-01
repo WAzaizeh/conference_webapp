42a285d9742acb2d0b38f1810621bf61e3af4877
wesam azaizeh <wesam.azaizeh@crant.ai>
2024-09-01 13:20:04 -0500

    Enabled CORS. Proper origins will be specified later.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
-from contextlib import asynccontextmanager
+from routers.limiter import limiter
 from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
+from fastapi.middleware.cors import CORSMiddleware
 from routers.events import router as events_router
 from routers.speakers import router as speakers_router
-from routers.limiter import limiter
-from slowapi import _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
+
 
 
 # @asynccontextmanager
@@ -13,6 +15,16 @@ from slowapi.errors import RateLimitExceeded
 
 
 app = FastAPI()  # FastAPI(lifespan=lifespan)
+
+# middleware to allow CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(events_router)
