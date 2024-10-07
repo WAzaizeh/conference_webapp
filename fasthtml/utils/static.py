b226a38ee62fe235fb09d653018cdc5ca0fc5e60
WAzaizeh <wesam.azaizeh@gmail.com>
2024-10-06 21:19:23 -0600

    Implemented cache busting.

--- fasthtml/utils/static.py
+++ fasthtml/utils/static.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+from datetime import datetime
 from fasthtml.common import Link, Script
+
+def cache_busting():
+    return datetime.now().strftime('%Y%m%d%H%M%S')
 
 def fetch_static_files():
     static_files = []
     for path in Path('./assets').rglob('*'):
         if path.is_file() and path.suffix == '.css':
-            static_files.append(Link(rel='stylesheet', href=f'/{path.name}', type='text/css'))
+            static_files.append(Link(rel='stylesheet', href=f'/{path.name}?t={cache_busting()}', type='text/css'))
         elif path.is_file() and path.suffix == '.js':
-            static_files.append(Script(src=f'/{path.name}'))
+            static_files.append(Script(src=f'/{path.name}?t={cache_busting()}'))
     return static_files
