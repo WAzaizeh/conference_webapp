76e1ff9305872a8dbccc597ccd2098eb2070ea41
WAzaizeh <wesam.azaizeh@gmail.com>
2025-10-13 22:53:46 -0500

    feat: updating loader io verifier

--- app/routes/loader_io.py
+++ app/routes/loader_io.py
@@ -2,10 +2,10 @@ from core.app import rt
 from fasthtml.common import *
 
 # Static file for loader.io verification (if file exists)
-@rt('/loaderio-6fe2ffb64dfc07be56f8da6e531010a2.txt')
+@rt('/loaderio-2eacf95a8c9f241ca8c2e18e3b2ca777.txt')
 def get():
     print('in loader io')
-    file_path = Path(__file__).parent / 'loaderio-6fe2ffb64dfc07be56f8da6e531010a2.txt'
+    file_path = Path(__file__).parent / 'loaderio-2eacf95a8c9f241ca8c2e18e3b2ca777.txt'
     if file_path.exists():
         return FileResponse(file_path)
-    return Response("loaderio-6fe2ffb64dfc07be56f8da6e531010a2", media_type='text/plain')
+    return Response("loaderio-2eacf95a8c9f241ca8c2e18e3b2ca777", media_type='text/plain')
